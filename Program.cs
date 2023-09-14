6960c56ea4a68d49ed8a52d0c7704bb756767b1c
Desantonio <vatsalgayakwad12@aol.com>
2023-09-14 13:27:01 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello from C# Script!");
+        Console.WriteLine("Hello, World!");
+        var a = 50;
+        Console.WriteLine(a);
+    }
+}
